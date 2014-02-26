48381b7e1154a2aa23581b0d630db36c5c6fde40
Yusuke Sakamoto <yus.sakamoto@gmail.com>
2014-02-26 03:07:10 -0600

    SVM classification implemented. problem -- very slow...

--- read_train.py
+++ read_train.py
@@ -3,52 +3,86 @@ import csv
 from sklearn import svm
 from sklearn import cross_validation
 import time
+from sklearn.externals import joblib
+
+def svm_vectors ( train, target, dim ):
+    "Get the SVN vectors for each cell"
+    clfs = []
+    start = time.time()
+    for i in range(dim):
+        print "%dth iteration" %i
+        st_svc = time.time()
+        clfs.append(svm.SVC())
+        clfs[i].fit(train, target[:,i])
+        ed_svc = time.time()
+        print "done, time: %f" %(ed_svc-st_svc)        
+    end = time.time()
+
+    print "ellapsed time = ", end - start
+
+    return clfs
+
+def svm_predict (clfs, data_test, dim ):
+    "Predict the values for each cell"
+    n_train, _ = data_test.shape
+    data_predict = np.zeros([dim, n_train])
+
+    for i in range(dim):
+        data_predict[i] = np.array(clfs[i].predict( data_test) )
+
+    return data_predict.T
+
 
 # read in  data, parse into training and target sets
+print "reading the dataset"
 dataset = np.genfromtxt(open('Data/train.csv','r'), delimiter=',', dtype='f8')[1:]
-
 n,m = dataset.shape
 
 # clean up the datasets
+print "cleaning up the dataset"
 id = np.zeros(n)
 delta = np.zeros(n)
 data_st = np.zeros([n,400])
 data_ed = np.zeros([n,400])
 for i in range(n):
-
     id[i] = int(dataset[i,0])
     delta[i] = int(dataset[i,1])
 
     data_st[i] = np.array(dataset[i,2:402],dtype=int)
     data_ed[i] = np.array(dataset[i,402:802],dtype=int)
 
-## data_ed: features
-## data_st[:][0]: class labels of the cell 0
+# run the svm
+print "runnning the svm"
+_, dim = data_st.shape
+# dim = 10
+n_train = n/5
+clfs = svm_vectors( data_ed[:n_train], data_st[:n_train,:], dim)
 
-n_train = n/10
+# save the clfs
+print "saving the clfs"
+joblib.dump(clfs, 'conway.pkl') 
 
-start = time.time()
+# predict the values
+print "predicting the values"
+data_predict = svm_predict( clfs, data_ed[n_train:n_train*2-1], dim)
 
-clf = svm.SVC()
-clf.fit(data_ed[:n_train], data_st[:n_train,0])
 
+error = sum((abs(data_st[n_train:n_train*2-1,0:dim] - data_predict)).T)
+print "error % = %f" %(sum(error)/((n_train-1)*dim))
 
-end = time.time()
-print "ellapsed time = ", end - start
+# ## the data sample too big... try smaller sample size.
+# cell_data = data_ed[:n_train]
+# cell_target = data_st[:n_train,0]
 
-## the data sample too big... try smaller sample size.
-cell_data = data_ed[:n_train]
-cell_target = data_st[:n_train,0]
+# ## cross validation
+# X_train, X_test, y_train, y_test = \
+# cross_validation.train_test_split(cell_data, cell_target, \
+#                                   test_size=0.4, random_state=0)
 
-## cross validation
-X_train, X_test, y_train, y_test = \
-cross_validation.train_test_split(cell_data, cell_target, \
-                                  test_size=0.4, random_state=0)
+# clf = svm.SVC(kernel='linear', C=1)
+# cv_scores = cross_validation.cross_val_score(clf, cell_data, cell_target, cv=5)
 
-clf = svm.SVC(kernel='linear', C=1)
-cv_scores = cross_validation.cross_val_score(clf, cell_data, cell_target, cv=5)
-
-print("Accuracy: %0.2f (+/- %0.2f)" % (cv_scores.mean(), cv_scores.std() * 2))
+# print("Accuracy: %0.2f (+/- %0.2f)" % (cv_scores.mean(), cv_scores.std() * 2))
 
 
 # import visualize_cells
@@ -56,4 +90,3 @@ print("Accuracy: %0.2f (+/- %0.2f)" % (cv_scores.mean(), cv_scores.std() * 2))
 
 # ds = data_st.reshape([20,20])
 # visualize_cells.visualize_cells(ds)
-
